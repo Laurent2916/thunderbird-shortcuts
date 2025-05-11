0cf43183b4c185c18c60a7766d96d4ba5e811fec
Laureηt <laurent.git@fainsin.bzh>
2025-05-11 13:32:07 +0200

    slightly improve genShortcut installPhase

--- flake.nix
+++ flake.nix
@@ -92,8 +92,7 @@
             zip -j @${id}.xpi logo.svg manifest.json script.js
 
             dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-            mkdir -p "$dst"
-            install -v -m644 @${id}.xpi $dst/@${id}.xpi
+            install -Dv -m644 @${id}.xpi $dst/@${id}.xpi
           '';
         };
     };
